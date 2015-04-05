9eb84e6be96d497b9018f1d9d0b9c0e67fd67375
Matt Healy <healmatt@gmail.com>
2015-04-05 09:05:11 +0800

    Remove unused manager command

--- manage.py
+++ manage.py
@@ -21,13 +21,5 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-@manager.command
-def fix_blank_entry_type():
-    """Fix the empty entry_type records."""
-    entries = Entry.query.all()
-    for entry in entries:
-        if entry.entry_type is None:
-            entry.entry_type = 1
-
 if __name__ == '__main__':
     manager.run()
