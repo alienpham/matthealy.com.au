70623ddb587799fa7ed85ac73bbf4e0e4e5325bc
Matt Healy <healmatt@gmail.com>
2015-05-03 16:04:53 +0800

    Sitemap generator

--- manage.py
+++ manage.py
@@ -27,5 +27,39 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def generate_sitemap():
+    """Generate an updated sitemap."""
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    filename = os.path.join(basedir, 'app/static/sitemap.xml')
+    target = open(filename,'w')
+
+    target.write("""<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>http://www.matthealy.com.au/</loc>
+  </url>
+  <url>
+    <loc>http://www.matthealy.com.au/services</loc>
+  </url>
+  <url>
+    <loc>http://www.matthealy.com.au/contact</loc>
+  </url>
+  <url>
+    <loc>http://www.matthealy.com.au/blog</loc>
+  </url>""")
+
+    posts = Post.query.filter_by(deleted=None).order_by(Post.timestamp.asc()).all()
+    for post in posts:
+        target.write("\n  <url>\n    <loc>http://www.matthealy.com.au/blog/post/" + post.slug + "</loc>\n  </url>")
+
+    tags = Tag.query.all()
+    for tag in tags:
+        target.write("\n  <url>\n    <loc>http://www.matthealy.com.au/blog/post/tagged/" + tag.name + "</loc>\n  </url>")
+
+    target.write("\n</urlset>")
+
+    target.close()
+
 if __name__ == '__main__':
     manager.run()
