9507c3441ace4e5c290af4c6b3a9d8fc9183e3c5
Matt Healy <healmatt@gmail.com>
2015-11-18 20:51:47 +0800

    Redirect on invalid contact form

--- app/main/views.py
+++ app/main/views.py
@@ -24,6 +24,9 @@ def submit_contact():
 
         return '', 200
 
+    else:
+        return redirect(url_for('main.index'))
+
 @main.route('/terms', methods=['GET'])
 def terms():
     return render_template("terms.html")
